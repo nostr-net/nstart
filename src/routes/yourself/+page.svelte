d0c3b70082962636f2cb611979471da933476448
dtonon <github@dtonon.com>
2024-09-30 15:31:32 +0200

    Add basic homepage

---
+++ src/routes/yourself/+page.svelte
@@ -0,0 +1,40 @@
+<div class="flex max-h-max bg-gradient-to-tr from-neutral-200 via-white to-white">
+	<div class="w-[48%] flex min-h-screen flex-none items-center justify-end">
+		<div class="w-full max-w-[350px] mr-10">
+			<!-- Welcome title -->
+			<div class="mb-8 border-l-[0.9rem] border-strongpink pl-4 sm:-ml-8">
+				<h1 class="font-bold">
+					<div class="text-[3em] leading-[1em] text-neutral-500 sm:text-[3em]">PRESENT</div>
+					<div class="break-words text-[3.5em] leading-[1em] sm:h-auto sm:text-[3.5em]" id="tw">
+						YOURSELF
+					</div>
+				</h1>
+			</div>
+
+			<!-- Intro text -->
+			<div class="w-[90%] text-[1.1rem] leading-5">
+				<p class="mb-6">On Nostr you decide to be whoever you want.</p>
+				<p class="mb-6">
+					A Nostr profile usually include a name, a picture and some additional information. Every
+					data is optional.
+				</p>
+
+				<p class="mb-6">
+					The name is not a unique username, we can have as many Jacks we want! Feel free to use
+					your real name or a nickname; you can change any info whenever you want.<br />
+					But remember: online privacy matters, don’t share sensitive data.
+				</p>
+
+				<p class="mb-6">
+					And yes, to join Nostr you don’t need to give your email, phone number or other data, it
+					is KYC free.
+				</p>
+			</div>
+		</div>
+	</div>
+	<div class="w-[52%] flex flex-none items-center justify-start bg-white">
+		<div class="w-full max-w-[350px] ml-16">
+			dsd
+		</div>
+	</div>
+</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        // 'base': '1.1rem',
+      },
+      colors: {
+        lavender: '#fdf0f5',
+        strongpink: '#e32a6d',
+        crimson: '#bc1150',
+        garnet: '#42091e'
+      },
+      lineHeight: {
+        normal: '0.8'
+      },
+    },
   },
   plugins: [],
 }
