a8a1905bcdefc97eb2be121c5d3d775c987f8524
dtonon <github@dtonon.com>
2024-10-31 11:28:12 +0100

    Add some noise to the background

--- src/lib/BasicLayout.svelte
+++ src/lib/BasicLayout.svelte
@@ -1,7 +1,12 @@
-<div class="max-h-max bg-gradient-to-tr from-neutral-200 via-white to-white">
-	<div class="flex min-h-screen items-center justify-center">
-		<div class="mx-auto p-8 sm:w-[860px]">
-			<slot name="content" />
+<div
+	class="max-h-max"
+	style="background: linear-gradient(to top right, rgba(220, 220, 220, 0.4), rgba(255, 255, 255, 0.9)), url('/bg/noisy.png');"
+>
+	<div class="">
+		<div class="flex min-h-screen items-center justify-center">
+			<div class="mx-auto p-8 sm:w-[860px]">
+				<slot name="content" />
+			</div>
 		</div>
 	</div>
 </div>
--- src/lib/TwoColumnLayout.svelte
+++ src/lib/TwoColumnLayout.svelte
@@ -1,7 +1,8 @@
 <div class="flex max-h-max flex-col sm:flex-row">
 	<!-- Intro block -->
 	<div
-		class="flex flex-none items-center justify-end bg-gradient-to-tr from-neutral-200 via-neutral-100 to-white p-8 sm:min-h-screen sm:w-[48%] sm:p-0"
+		class="flex flex-none items-center justify-end p-8 sm:min-h-screen sm:w-[48%] sm:p-0"
+		style="background: linear-gradient(to top right, rgba(220, 220, 220, 0.5), rgba(255, 255, 255, 0.95)), url('/bg/noisy.png');"
 	>
 		<slot name="intro" />
 	</div>
