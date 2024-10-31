fffa1f6e5053573615fab4289edce1ed664a0ad5
dtonon <github@dtonon.com>
2024-10-31 12:52:58 +0100

    Add a pretty image to the homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,49 +2,80 @@
 	import BasicLayout from '$lib/BasicLayout.svelte';
 </script>
 
-<BasicLayout>
-	<div slot="content">
-		<!-- Welcome title -->
-		<div class="mb-8 border-l-[0.9rem] border-strongpink pl-4 sm:-ml-8">
-			<h1 class="font-bold">
-				<div class="text-[3rem] leading-[1em] text-neutral-500 sm:text-[6rem]">WELCOME</div>
-				<div class="break-words text-[3.5rem] leading-[1em] sm:h-auto sm:text-[7rem]" id="tw">
-					<span class="text-neutral-400">TO</span> NOSTR
+<div
+	class="max-h-max"
+	style="background: linear-gradient(to top right, rgba(220, 220, 220, 0.4), rgba(255, 255, 255, 0.9)), url('/bg/noisy.png');"
+>
+	<div>
+		<div class="flex min-h-screen items-center justify-center">
+			<div class="mx-auto">
+				<!-- content-->
+				<div class="p-8 sm:flex sm:flex-row-reverse sm:p-0">
+					<div
+						class="flex justify-center sm:relative sm:w-[45vw] sm:overflow-hidden"
+					>
+						<img
+							src="/images/relay.png"
+							class="z-0 mb-8 w-60 object-cover sm:absolute sm:w-full sm:object-left sm:-right-[20px] sm:top-10"
+							alt="Nostr Client"
+						/>
+					</div>
+
+					<div class="z-20 sm:w-[55vw] sm:min-w-[720px] sm:pl-[16vw] sm:py-10">
+						<!-- Welcome title -->
+						<div class="mb-8 border-l-[0.9rem] border-strongpink pl-4 sm:-ml-8">
+							<h1 class="font-bold">
+								<div class="text-[3rem] leading-[1em] sm:text-[5rem]">WELCOME</div>
+								<div
+									class="break-words text-[3.5rem] leading-[1em] sm:h-auto sm:text-[6rem]"
+									id="tw"
+								>
+									<span class="text-neutral-500">TO</span> <span class="text-strongpink">NOSTR</span>
+								</div>
+							</h1>
+						</div>
+
+						<!-- Intro text -->
+						<div class="text-[1.3rem] leading-7 text-neutral-700">
+							<p class="">
+								To join Nostr you need a “profile”, but it is not the usual one that a company
+								generates and manages for you. You manage the protocol, you manage your identity.
+							</p>
+							<p class="mt-6">
+								Nostr is a different and freedom experience from the beginning: it empowers user
+								using cryptographic key-pairs that don’t require a third part; don’t worry about the
+								tech slang, it is just a strong password that you will have to keep safe.
+							</p>
+							<p class="mt-6">
+								This wizard is one of the many way to bootstrap a Nostr profile that you can use
+								anywhere, we help you to create you key-pair and safely manage it in 3 easy steps.
+								Are you ready?
+							</p>
+						</div>
+
+						<!-- Start button -->
+						<div class="my-8 sm:-mr-20 flex justify-center sm:justify-end">
+							<a
+								class="inline-flex items-center rounded bg-strongpink px-10 py-3 text-[1.6rem] text-white"
+								href="/yourself"
+							>
+								Let's Start <img
+									src="/icons/arrow-right.svg"
+									alt="Icon"
+									class="ml-4 mr-2 h-8 w-8"
+								/>
+							</a>
+						</div>
+
+						<!-- Footer -->
+						<div class="leading-6 text-neutral-500">
+							Would you like to know more about Nostr, first?<br class="hidden sm:inline-block" />
+							<a href="https://njump.me" class="underline">Read a quick introduction</a>
+						</div>
+					</div>
 				</div>
-			</h1>
-		</div>
-
-		<!-- Intro text -->
-		<div class="text-[1.3rem] leading-7 text-neutral-700 sm:w-[80%]">
-			<p class="">
-				To join Nostr you need a “profile”, but it is not the usual one that a company generates and
-				manages for you. You manage the protocol.
-			</p>
-			<p class="mt-6">
-				Nostr is a different and freedom experience from the beginning: it empowers user using
-				cryptographic key-pairs that don’t require a third part; don’t worry about the tech slang,
-				it is just a strong password that you will have to keep safe.
-			</p>
-			<p class="mt-6">
-				This wizard is one of the many way to bootstrap a Nostr profile that you can use anywhere,
-				we help you to create you key-pair and safely manage it in 3 easy steps. Are you ready?
-			</p>
-		</div>
-
-		<!-- Start button -->
-		<div class="my-8 flex justify-center sm:justify-end">
-			<a
-				class="inline-flex items-center rounded bg-strongpink px-10 py-3 text-[1.6rem] text-white"
-				href="/yourself"
-			>
-				Let's Start <img src="/icons/arrow-right.svg" alt="Icon" class="ml-4 mr-2 h-8 w-8" />
-			</a>
-		</div>
-
-		<!-- Footer -->
-		<div class="leading-6 text-neutral-500">
-			Would you like to know more about Nostr, first?<br class="hidden sm:inline-block" />
-			<a href="https://njump.me" class="underline">Read a quick introduction</a>
+				<!-- /content -->
+			</div>
 		</div>
 	</div>
-</BasicLayout>
+</div>
