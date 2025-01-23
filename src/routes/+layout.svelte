276cf288d9791299eaff2907b8dd0ac7d58281cd
dtonon <github@dtonon.com>
2025-01-23 17:10:52 +0100

    Remove useless code

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,24 +6,6 @@
 	// Check if we're in iframe mode
 	let isModal = browser && window.self !== window.top;
 	setContext('isModal', isModal);
-
-	// Handle completion in iframe mode
-	function handleComplete(result: any) {
-		if (isModal && browser) {
-			window.parent.postMessage(
-				{
-					type: 'WIZARD_COMPLETE',
-					result
-				},
-				'*'
-			);
-		}
-	}
-
-	// Make it available globally
-	if (browser) {
-		(window as any).handleComplete = handleComplete;
-	}
 </script>
 
 {#if isModal}
